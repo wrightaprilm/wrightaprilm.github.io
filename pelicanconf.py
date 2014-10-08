283db0adcc8757a51f5094a58602bcd4d1b83aa8
April Wright <wright.aprilm@gmail.com>
2014-10-08 12:33:43 -0500

    updated new paper

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,26 +4,27 @@ from __future__ import unicode_literals
 
 AUTHOR = u'April Wright'
 SITENAME = u'Wrighting April'
-SITEURL = ''
+SITEURL = 'http://wrightaprilm.github.io/'
 
+FEED_ALL_RSS = 'indexrss.xml'
+FEED_ALL_ATOM = 'index.xml'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
 GITHUB_LINK='https://github.com/wrightaprilm'
 DISQUS_SITENAME='wrightaprilm'
-THEME='~/pelican-themes/nmnlist/'
+THEME = '/home/april/pelican-themes/nmnlist/'
 # Social widget
 SOCIAL = (('Twitter', 'twitter.com/wrightingapril'),
-          ('Another social link', '#'),)
+          ('Github', 'https://github.com/wrightaprilm'),)
 
-DEFAULT_PAGINATION = 5
+DEFAULT_PAGINATION = 3
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
